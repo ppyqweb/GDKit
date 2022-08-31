5b19f5f29ea1987f79f30f7768ec1edee021ca53
liaogd <285282227@qq.com>
2022-08-31 18:39:37 +0800

    修改GDGestureTableView

--- GDKit/Classes/GDTableView/GDGestureTableView.swift
+++ GDKit/Classes/GDTableView/GDGestureTableView.swift
@@ -16,12 +16,38 @@ open class GDGestureTableView:  UITableView {
 extension GDGestureTableView: UIGestureRecognizerDelegate {
     
     public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
         // 仅仅让HomeVC的tableFooterView区域支持多个手势共存, 解决分页以上部分的"横向滑动视图(嵌套UICollectionView)"和scrollView的纵向滑动的手势冲突问题
-        let currentPoint = gestureRecognizer.location(in: self)
-        let segmentViewContentScrollViewHeight = self.tableFooterView?.frame.size.height ?? 0
-        let frame = CGRect(x: 0, y: self.contentSize.height - segmentViewContentScrollViewHeight, width: UIScreen.main.bounds.size.width, height: segmentViewContentScrollViewHeight)
-        let isContainsPoint = frame.contains(currentPoint)
-        return isContainsPoint ? true : false
+        //        let currentPoint = gestureRecognizer.location(in: self)
+        //        let segmentViewContentScrollViewHeight = self.tableFooterView?.frame.size.height ?? 0
+        //        let frame = CGRect(x: 0, y: self.contentSize.height - segmentViewContentScrollViewHeight, width: UIScreen.main.bounds.size.width, height: segmentViewContentScrollViewHeight)
+        //        let isContainsPoint = frame.contains(currentPoint)
+        //        return isContainsPoint ? true : false
+    }
+    
+    open override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+}
+
+
+
+open class GDGestureScrollView:  UIScrollView {
+    
+}
+
+
+extension GDGestureScrollView: UIGestureRecognizerDelegate {
+    
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+        // 仅仅让HomeVC的tableFooterView区域支持多个手势共存, 解决分页以上部分的"横向滑动视图(嵌套UICollectionView)"和scrollView的纵向滑动的手势冲突问题
+        //        let currentPoint = gestureRecognizer.location(in: self)
+        //        let segmentViewContentScrollViewHeight = 0.0 //self.tableFooterView?.frame.size.height ?? 0
+        //        let frame = CGRect(x: 0, y: self.contentSize.height - segmentViewContentScrollViewHeight, width: UIScreen.main.bounds.size.width, height: segmentViewContentScrollViewHeight)
+        //        let isContainsPoint = frame.contains(currentPoint)
+        //        return isContainsPoint ? true : false
     }
     
     open override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
